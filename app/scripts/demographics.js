7323797fa3ff20cbd0d60e9043fd54bc54b63ab8
becca <brickaa@users.noreply.github.com>
2016-03-03 17:49:45 -0600

    charts with resize/scroll working

--- app/scripts/demographics.js
+++ app/scripts/demographics.js
@@ -68,100 +68,108 @@
 
           var dataUT = [],
               dataTX = [],
-              dataRace = [];
+              dataRace = [],
+              dataRaceTX = [],
+              dataRaceUT = [];
 
           dataUT = data[0].values;
           dataTX = data[1].values;
-
-          dataRace.push(dataTX[index]);
-
-          // svg.append('g')
-          //     .attr('class', 'x axis')
-          //     .attr('transform', 'translate(0,' + height + ')')
-          //     .call(xAxis);
+          dataRace.push(dataTX[index], dataUT[index]);
+          dataRaceTX.push(dataTX[index]);
+          dataRaceUT.push(dataUT[index]);
 
           svg.append('g')
               .attr('class', 'y axis')
               .call(yAxis.ticks('5', '%'));
 
-          svg.selectAll('.bar')
-            .data(dataRace, function(d) {
-              return d.values[0].group;
-            })
-            .enter().append('rect')
-              .attr('class', 'bar')
-              .attr('x', function(d, i) { return x(d.values[i].date) + (width/4 * (i) + 4); })
-              .attr('width', width/4)
-              .attr('y', function(d, i) { return y(d.values[i].percent); })
-              .attr('height', function(d, i) { return y(0) - y(d.values[i].percent); })
-              .attr('fill', function(d, i) { return color(d.values[i].group); });
-
           d3.select(window).on('resize.' + race, resize).transition();
 
-          function addUTBar() {
-            dataRace.push(dataUT[index]);
+          function removeBars() {
+            var bars = svg.selectAll('rect');
+            bars.remove();
+          }
 
-            svg.selectAll('.bar')
-              .data(dataRace, function(d) {
-                return d.values[1].group;
-              })
-              .enter().append('rect')
+          function removeXAxis() {
+            var axis = svg.select('.x.axis');
+            axis.remove();
+          }
+
+          function removeSeries() {
+            var series = svg.selectAll('.group');
+            series.remove();
+          }
+
+          function addBars(data) {
+            if (data === 'dataRaceTX') {
+              removeBars();
+            }
+
+            var bars = svg.selectAll('.bar')
+              .data(data, function(d) {
+                return d.values[0].group;
+              });
+          
+            bars.exit()
+              .transition()
+                .duration(300)
+              .attr('y', y(0))
+              .attr('height', height - y(0))
+              .style('fill-opacity', 1e-6)
+              .remove();
+
+            // data that needs DOM = enter() (a set/selection, not an event!)
+            bars.enter().append('rect')
                 .attr('class', 'bar')
-                .attr('x', function(d, i) { return x(d.values[i].date) + ((width/4) * (i) + 4); })
-                .attr('width', width/4)
-                .attr('y', function(d, i) { return y(d.values[i].percent); })
-                .attr('height', function(d, i) { return y(0) - y(d.values[i].percent); })
-                .attr('fill', function(d, i) { return color(d.values[i].group); });
+                .attr('fill', function(d, i) { return color(d.values[i].group); })
+                .attr('y', y(0))
+                .attr('height', height - y(0));
 
+            // the 'UPDATE' set:
+            bars.transition().duration(1000)
+              .attr('x', function(d, i) { return x(d.values[i].date) + (width/4 * (i) + 4); })              .attr('width', width/4)
+              .attr('y', function(d, i) { return y(d.values[i].percent); })
+              .attr('height', function(d, i) { return y(0) - y(d.values[i].percent); }); // flip the height, because y's domain is bottom up, but SVG renders top down
           }
 
-          function addTXLine() {
+          function addXAxis() {
+            svg.select('.x.axis').remove();
+            svg.append('g')
+                .attr('class', 'x axis')
+                .attr('transform', 'translate(0,' + height + ')')
+                .call(xAxis);
+          }
 
-            if(yDomain && yDomain === 1) {
-              resetScale();
-            } else if (yDomain) {
-              rescale();
-            }
+          function addSeries(addData) {
+            removeBars();
+            removeSeries();
+
+            // if(yDomain && yDomain === 1) {
+            //   resetScale();
+            // } else if (yDomain) {
+            //   rescale();
+            // }
 
             var series = svg.selectAll('.group')
-                .data(dataRace, function(d, i) {
+                .data(addData, function(d, i) {
                   return d.values[i].group;
                 })
                 .enter().append('g')
                   .attr('class', 'group');
 
-            series.append('path')
+            var path = series.append('path')
                 .attr('class', 'line')
                 .attr('d', function(d) { 
                     return line(d.values);
                 })
                 .style('stroke', function(d, i) { return color(d.values[i].group); });
-
-
-          }
-
-          function addUT() {
-            dataRace.push(dataUT[index]);
-
-            if(yDomain && yDomain === 1) {
-              resetScale();
-            } else if (yDomain) {
-              rescale();
-            }
-
-            var series = svg.selectAll('.group')
-                .data(dataRace, function(d, i) {
-                  return d.values[i].group;
-                })
-                .enter().append('g')
-                  .attr('class', 'group');
-
-            series.append('path')
-                .attr('class', 'line')
-                .attr('d', function(d) { 
-                    return line(d.values);
-                })
-                .style('stroke', function(d, i) { return color(d.values[i].group); });
+     
+           path.each(function(d) { d.totalLength = this.getTotalLength(); })
+               .attr('stroke-dasharray', function(d) { return d.totalLength + ' ' + d.totalLength; })
+               .attr('stroke-dashoffset', function(d) { return d.totalLength; })
+               .transition()
+                 .duration(1000)
+                 .ease('linear')
+                 .attr('stroke-dashoffset', 0);
           }
 
           function resize() {
@@ -172,27 +180,36 @@
               .attr('width', width + margin.left + margin.right);
 
             // // resize the chart
-            x.range([0,width]);
+            x.range([0, width]);
 
             xAxis = d3.svg.axis()
               .scale(x)
               .orient('bottom');
 
             // update xAxis, data
-            // svg.select('g')
-            //   .attr('class', 'x axis')
-            //   .attr('transform', 'translate(0,' + height + ')')
-            //   .call(xAxis);
+            svg.select('.x.axis')
+              .attr('transform', 'translate(0,' + height + ')')
+              .call(xAxis);
 
             var series = svg.selectAll('.group');
 
-            series.select('path')
+            series.select('path').remove();
+
+            var path = series.append('path')
               .attr('class', 'line')
               .attr('d', function(d) { 
                   return line(d.values);
               })
               .style('stroke', function(d, i) { return color(d.values[i].group); });
           
+            path.each(function(d) { d.totalLength = this.getTotalLength(); })
+              .attr('stroke-dasharray', function(d) { return d.totalLength + ' ' + d.totalLength; })
+              .attr('stroke-dashoffset', function(d) { return d.totalLength; })
+              .transition()
+                .duration(1000)
+                .ease('linear')
+                .attr('stroke-dashoffset', 0);
+
             var bars = svg.selectAll('rect');
 
             bars.attr('x', function(d, i) { return x(d.values[i].date) + ((width/4) * (i) + 4); })
@@ -256,12 +273,73 @@
               .style('stroke', function(d, i) { return color(d.values[i].group); });          
           }
 
+          var inview1 = new Waypoint.Inview({
+            element: $('#waypoint1')[0],
+            enter: function(direction) {
+              if (direction === 'down') {
+                console.log('enter down 1');
+                addBars(dataRaceTX);
+                removeXAxis();
+              } 
+            },
+            exit: function(direction) {
+              if (direction === 'up') {
+                console.log('exit up 1');
+                removeBars();
+                removeXAxis();
+              }
+            }
+          });
+
           var inview2 = new Waypoint.Inview({
             element: $('#waypoint2')[0],
-            exit: function(direction) {
+            enter: function(direction) {
               if (direction === 'down') {
-                addUTBar();
-              } else if (direction === 'up') {
+                console.log('enter down 2');
+                addBars(dataRace);
+                removeXAxis();
+              }
+            },
+            exit: function(direction) {
+              if (direction === 'up') {
+                console.log('exit up 2');
+                addBars(dataRaceTX);
+                removeXAxis();
+              }
+            }
+          });
+
+          var inview3 = new Waypoint.Inview({
+            element: $('#waypoint3')[0],
+            enter: function(direction) {
+              if (direction === 'down') {
+                console.log('enter down 3');
+                addXAxis();
+                addSeries(dataRaceTX);
+              }
+            },
+            exit: function(direction) {
+              if (direction === 'up') {
+                console.log('exit up 3');
+                addBars(dataRace);
+                removeSeries();
+                removeXAxis();
+              }
+            }
+          });
+
+          var inview4 = new Waypoint.Inview({
+            element: $('#waypoint4')[0],
+            enter: function(direction) {
+              if (direction === 'down') {
+                console.log('enter down 4');
+                addSeries(dataRace);
+              }
+            },
+            exit: function(direction) {
+              if (direction === 'up') {
+                console.log('exit up 4');
+                addSeries(dataRaceTX);
               }
             }
           });
@@ -293,7 +371,7 @@
 
   var inview = new Waypoint.Inview({
     element: $('#waypoint1')[0],
-    exit: function(direction) {
+    enter: function(direction) {
       if (direction === 'down') {
         text1.animate({ opacity: 1});
       } else if (direction === 'up') {
@@ -304,15 +382,40 @@
 
   var inview2 = new Waypoint.Inview({
     element: $('#waypoint2')[0],
-    exit: function(direction) {
+    enter: function(direction) {
       if (direction === 'down') {
         text1.animate({ opacity: 0});
         text2.animate({ opacity: 1});
-        console.log('exit 2');
         $('.legend_ut').animate({ opacity: 1 });
       } else if (direction === 'up') {
         text1.animate({ opacity: 1});
         text2.animate({ opacity: 0});
+      }
+    }
+  });
+
+  var inview3 = new Waypoint.Inview({
+    element: $('#waypoint3')[0],
+    enter: function(direction) {
+      if (direction === 'down') {
+        text2.animate({ opacity: 0});
+        text3.animate({ opacity: 1});
+      } else if (direction === 'up') {
+        text2.animate({ opacity: 1});
+        text3.animate({ opacity: 0});
+      }
+    }
+  });
+
+  var inview2 = new Waypoint.Inview({
+    element: $('#waypoint2')[0],
+    enter: function(direction) {
+      if (direction === 'down') {
+        text3.animate({ opacity: 0});
+        text4.animate({ opacity: 1});
+      } else if (direction === 'up') {
+        text3.animate({ opacity: 1});
+        text4.animate({ opacity: 0});
       }
     }
   });
